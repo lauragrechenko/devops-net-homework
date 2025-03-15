27cc9e3034c14b1782ef7494f53d0d0d83ff956d
lauragrechenko <lauragrechenko@gmail.com>
2025-03-15 22:34:50 +0100

    [HOTFIX] terr-hw-5 task 3

--- ter-homeworks-05/src-2/modules/doc_table/main.tf
+++ ter-homeworks-05/src-2/modules/doc_table/main.tf
@@ -1,3 +1,9 @@
+resource "yandex_kms_symmetric_key" "dynamodb_key" {
+  name            = var.dynamodb_key_name
+  description     = var.dynamodb_key_description
+  rotation_period = var.dynamodb_key_rotation_period
+}
+
 resource "aws_dynamodb_table" "state_lock_table" {
   name         = var.dynamodb_table_name
   billing_mode = var.dynamodb_table_billing_mode
@@ -6,5 +12,15 @@ resource "aws_dynamodb_table" "state_lock_table" {
   attribute {
     name = var.dynamodb_table_attribute_name
     type = var.dynamodb_table_attribute_type
+  }
+
+  server_side_encryption {
+    enabled = var.dynamodb_table_sse_enabled
+    # have to construct it manually
+    kms_key_arn = "arn:yc:kms:${var.aws_region}:${var.cloud_id}:key/${yandex_kms_symmetric_key.dynamodb_key.id}"
+  }
+
+  point_in_time_recovery {
+    enabled = var.dynamodb_table_pitr_enabled
   }
 }
